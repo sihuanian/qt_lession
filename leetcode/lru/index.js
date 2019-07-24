6597108ba29dc979a7047ecfd59727b90ec36a64
sihuanian <2267322300@qq.com>
2019-07-24 15:11:42 +0800

    lru完成

--- leetcode/lru/index.js
+++ leetcode/lru/index.js
@@ -36,4 +36,39 @@ put(3,3) [1,3] 带来空间分配的 回收空间 最近最少使用（length >=
 
 
 
-*/
+// */
+// var LRUCache = function (capacity) {
+//     this.capacity = capacity
+//     this.map = new Map()
+// };
+
+// /** 
+//  * @param {number} key
+//  * @return {number}
+//  */
+// LRUCache.prototype.get = function (key) {
+//     let value = this.map.get(key)
+
+//     if (this.map.has(key)) {
+//         this.map.delete(key)
+//         this.map.set(key, value)
+//         return value
+//     }
+//     return -1
+// };
+
+// /** 
+//  * @param {number} key 
+//  * @param {number} value
+//  * @return {void}
+//  */
+// LRUCache.prototype.put = function (key, value) {
+//     if (this.map.has(key)) {
+//         this.map.delete(key)
+//     }
+//     this.map.set(key, value)
+//     if (this.map.size > this.capacity) {
+//         let k = this.map.keys().next().value
+//         this.map.delete(k)
+//     }
+// };
