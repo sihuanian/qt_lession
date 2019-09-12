76e7d220cc074260d27fb1313a99010fc9fef116
sihuanian <2267322300@qq.com>
2019-09-12 21:30:16 +0800

    自定义组件

---
+++ vue/carousel/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <!-- v-bind "" 内部是js 1000代表number -->
+    <EcSlide :time="1000" :autoplay="true" :list="list" :type="'slide'" :option="true"
+      :swidth="800" :slidetype="'ease'" :direction="'left'" />
+  </div>
+</template>
+
+<script>
+import EcSlide from './components/EcSlide'
+export default {
+  name: 'App',
+  components: { EcSlide },
+  data() {
+    return {
+      // 父子组件数据传递
+      list: [
+        {
+          href: 'https://36kr.com/p/5245067',
+          src: 'https://pic.36krcnd.com/201909/12030204/a6myzaob5uyszomn!heading'
+        },
+        {
+          href: 'https://36kr.com/p/5245731',
+          src: 'https://pic.36krcnd.com/201909/12000217/fggsu2ypi9t4011a!heading'
+        },
+        {
+          href: 'https://36kr.com/p/5245740',
+          src: 'https://pic.36krcnd.com/201909/11234002/cdcyqxr4mh1lrexo!heading'
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue/carousel/src/components/EcSlide.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="switch-img-box" id="ec-slide-box" :style="{width: swidth + 'px'}">
+    幻灯片
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EcSlide', // 自定义组件必须加入
+  // 组件内部数据
+  data () {
+    return {
+      timer: null, // 定时器 组件自己打理
+    }
+  },
+  // 外界的传参
+  // ['time', 'autoplay']
+  props: {
+    time: {
+      type: Number,
+      default: 4000
+    },
+    autoplay: {
+      type: Boolean,
+      default: true
+    },
+    swidth: {
+      type: Number
+    }
+  },
+  computed: {
+    
+  },
+  methods: {
+    
+  },
+  mounted() {
+    
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// stylus stylus-loader
+.switch-img-box
+  width 100%
+  height 100%
+  position relative
+</style>
