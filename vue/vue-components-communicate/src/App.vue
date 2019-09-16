e61b0e3d94e7ca76955ea82f9a0472695bbb70ed
sihuanian <2267322300@qq.com>
2019-09-16 19:21:32 +0800

    '组件间信息传递'

---
+++ vue/vue-components-communicate/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Child1 :msg="msg" :msg2="msg2" :fn="appFn" v-bind="test" :count="count"
+       @addCount="countAdd" :add="countAdd" />
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import Child1 from './components/common/child1'
+export default {
+  name: 'App',
+  components: {
+    Child1
+  },
+  data() {
+    return {
+      msg: '给child1的礼物',
+      msg2: '再次给child1的礼物',
+      test: {
+        a: 1,
+        b: 2,
+        c: {
+          d: 4
+        }
+      },
+      count: 1
+    }
+  },
+  methods: {
+    appFn () {
+      console.log('我是App.vue 里面的方法')  
+    },
+    countAdd () {
+      this.count++
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ vue/vue-components-communicate/src/components/common/child1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="child1">
+    child1<br>
+    msg: {{ msg }}<br>
+    msg2: {{ msg2 }}<br>
+    <button @click="fn">使用父组件中的方法</button><br>
+    {{ a }}  {{ b }}  {{ c.d }}<br>
+    <button @click="add">+</button>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg', 'msg2', 'fn', 'a', 'b', 'c', 'count', 'add'],
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    // add () {
+    //   // this.count++
+    //   this.$emit('addCount')
+    // }
+  },
+  created() {
+    this.oCount = this.count
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
