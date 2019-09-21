3a761188da3b3893b029b35831b8a0cadfa70a5c
sihuanian <2267322300@qq.com>
2019-09-21 23:37:52 +0800

    menu组件

---
+++ vue/menu/README.md
@@ -0,0 +1,14 @@
+- <slot /> 插槽
+  <Menu>
+
+  </Menu>
+
+- 像设计盒子一样设计组件吧
+  <Menu> 容器组件
+  只有html, css style-component
+
+- template 就高效设计的一个法宝
+  承载指令 用的舒服
+
+- 组件中有多个slot
+  给slot 添加name="title" 属性，引用这个组件加上 #title
---
+++ vue/menu/src/components/SubMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="subMenu">
+    <span @click="change">
+      <i v-show="!isShow">+</i>
+      <i v-show="isShow">-</i>
+      <!-- slot -->
+      <slot name="title"></slot>
+    </span>
+    <li v-show="isShow">
+      <slot></slot>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false
+    };
+  },
+  methods: {
+    change () {
+      this.isShow = !this.isShow
+    }
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+  .subMenu 
+    span 
+      display block
+      padding 5px
+      background #4cb4e7
+      color #fff
+      border-bottom 1px solid #fff
+      position relative
+      i
+        position absolute
+        right 10px
+        bottom 5px
+        font-style normal
+        font-weight bold
+
+  li
+    background #e2dbb2
+</style>
