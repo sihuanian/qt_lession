f5d174629b47f14aa5b9dfc2ff2abd3a28572f34
sihuanian <2267322300@qq.com>
2019-09-25 23:11:59 +0800

    html2canvas

---
+++ vue/vue-flash-image-A/README.md
@@ -0,0 +1,4 @@
+## 安装sass 编译
+
+> npm install -D sass-loader@7.3.1  node-sass
+
---
+++ vue/vue-flash-image-A/src/components/Main.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <div class="gauntlet">
+      <button @click="disAppear">消失</button>
+    </div>
+    <Content :heroes="heroes" :lostHeroId="lostHeroId" />
+  </div>
+</template>
+
+<script>
+import { heroes } from './heroes'
+import Content from './Content'
+export default {
+  data() {
+    return {
+      heroes,
+      lostHeroId: []
+    }
+  },
+  components: {
+    Content
+  },
+  methods: {
+    disAppear () {
+      this.lostHeroId = this.heroes.slice(0).sort(() => 0.5 - Math.random())
+        .slice(0, Math.ceil(this.heroes.length / 2))
+        .map(hero => hero.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
