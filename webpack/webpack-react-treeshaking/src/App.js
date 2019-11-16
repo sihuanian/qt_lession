8981bd84748edc3b4bde7e28a1f94ca01892b3de
sihuanian <2267322300@qq.com>
2019-11-16 23:50:20 +0800

    css-loader,静态资源文件，热启动，css模块化配置

--- webpack/webpack-react-treeshaking/src/App.js
+++ webpack/webpack-react-treeshaking/src/App.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import { hot } from 'react-hot-loader';
+import React, { useState } from 'react'
+import { hot } from 'react-hot-loader'
+// import './app.scss'
+import styleObj from './app.scss' // 引入模块化样式
 
 class App extends React.Component {
 	render () {
 		return (
 			<div>
-        123456
+				{/* 使用模块化的样式 */}
+				<p className={styleObj.item}>7416</p>
+				<h2>我在等世上唯一契合的灵魂</h2>
+				<img src="images/83667-1920x1200.jpg" alt="公共资源"/>
+        1234
 			</div>
 		)
 	}
 }
-
-export default hot(module)(App);
+// app下面更改，会热启动
+export default hot(module)(App)
 // export default App;
--- webpack/webpack-react-treeshaking/webpack.config.js
+++ webpack/webpack-react-treeshaking/webpack.config.js
@@ -1,18 +1,60 @@
 const path = require('path');
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin') // dist生成对应的文件夹
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 分离出css
 
 module.exports = {
+  // 入口文件
+  entry: [
+    'react-hot-loader/patch',
+    path.resolve(__dirname, './src/index.js')
+  ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: ['babel-loader'],
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader, // 将 JS 字符串生成为 style 节点
+          options: {
+            hmr: true
+          }
+        }, {
+          loader: "css-loader", // 将 CSS 转化成 CommonJS 模块
+          options: {
+            // modules: true // 开启css 模块化，类似于vue scoped
+            modules: {
+              mode: 'local', // 开启css 模块化，类似于vue scoped
+              localIdentName: '[path][name][hash:base64:5]'
+            }
+          }
+        }, {
+          loader: "sass-loader" // 将 Sass 编译成 CSS
+        }]
+      }
+    ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './assets/imgaes',
+        to: 'assets/'
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
     })
-  ]
+  ],
+  devServer: {
+    hot: true, // 启动热更新
+    contentBase: path.join(__dirname, './assets/')
+  }
 }
